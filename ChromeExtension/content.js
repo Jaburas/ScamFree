7c2dfeabd3827cbbf04cf4f22bd4191b48cc3003
fannytraore12 <fannytraore002@gmail.com>
2026-02-01 12:15:05 -0500

    Update content.js

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,6 +1,7 @@
 console.log("Scam detector running");
 let lastEmailtext = "";
 //scanning email and sending it to AI
+//This code was debugged by chat gtp
 async function scanEmail(emailText) {
     return new Promise((resolve) => {
         chrome.runtime.sendMessage(
@@ -26,24 +27,32 @@ const observer = new MutationObserver(()=>{
 
     console.log("Email opened:");
     console.log("Email text:", text);
+    //Create banner if its not on screen!
     if(document.getElementById("scam-warning"))return;
     const warning = document.createElement("div");
     warning.id = "scam-warning";
     warning.innerText = "This email is being checked for scams.";
 
     scanEmail(text).then(result => {
-        console.log(result);
-        if(result.label === "scam"){
-            warning.innerText =`Scam detected(${Math.round(result.confidence*100)}%)`;
-            warning.style.background = "#f8d7da";
-            warning.style.color ="#842029";
-        }else{
-            warning.innerText =`Looks safe!(${Math.round(result.confidence*100)}%)`;
-            warning.style.background = "#d1e7dd";
-            warning.style.color ="#0f5132";
-
-        }
-    })
+    console.log("Result from background:", result);
+    
+    // 1. Convert to lowercase to be safe
+    // 2. Check for BOTH "scam" and "spam" labels cuz Ai checks for both
+    const label = result.label ? result.label.toLowerCase() : "";
+    const isSuspicious = (label === "scam" || label === "spam");
+    //Turn percentage
+    const confidence = result.confidence ? Math.round(result.confidence * 100) : 0;
+    //Show the banner depending on the result
+    if (isSuspicious) {
+        warning.innerText = `Scam  or Spam detected (${confidence}%)`;
+        warning.style.background = "#f8d7da";
+        warning.style.color = "#842029";
+    } else {
+        warning.innerText = `Looks safe! (${confidence}%)`;
+        warning.style.background = "#d1e7dd";
+        warning.style.color = "#0f5132";
+    }
+});
 
     warning.style.background = "#6FB5E7";
     warning.style.color = "#334D84";
